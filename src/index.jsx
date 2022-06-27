ca42d488bb712b06990759a722e267e1a2968d63
kazachenko2v <kazachenko2v@gmail.com>
2022-06-27 16:20:48 +0300

    package.json fix

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,5 @@
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import "./styles/reset.css";
 import "./styles/App.css";
 
