d81cd536fcecb6796b2c80b8e1e171736dc47ef2
kazachenko2v <kazachenko2v@gmail.com>
2023-03-26 09:45:29 +0300

    qwe

--- src/pages/Account/Account.tsx
+++ src/pages/Account/Account.tsx
@@ -41,7 +41,7 @@ const Account: React.FC = () => {
           (snapshot) => {
             const progress =
               (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-            console.log("Upload is " + progress + "% done");
+            // console.log("Upload is " + progress + "% done");
             setPerc(progress);
           },
           (error) => {
